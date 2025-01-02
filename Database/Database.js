33f6c276df9ac63b30e82090c1a64b23c9d7d18f
Your Name <your.email@example.com>
2025-01-02 18:41:11 +0530

    update

--- Database/Database.js
+++ Database/Database.js
@@ -1,20 +1,41 @@
+// const mongoose = require("mongoose");
+
+// async function connectDB() {
+//   try {
+//     const client = await mongoose.connect(
+//       "mongodb+srv://shahul1145:Shahul@123@cluster0.q83rynw.mongodb.net/tracker" ,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       }
+//     );
+//     console.log("MongoDB connected successfully!");
+//   } catch (error) {
+//     console.log(error);
+//     console.log("MongoDB connection failed");
+//   }
+// }
+// module.exports = connectDB;
+
+// // "mongodb://127.0.0.1:27017/expenseTracker" pass:923IKYHSTB3Reazz
+
+
 const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    const client = await mongoose.connect(
-      "mongodb://0.0.0.0:27017/mini-Ecommerce" ,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+
+    const uri = "mongodb+srv://shahul1145:Shahul%40123@cluster0.q83rynw.mongodb.net/tracker?retryWrites=true&w=majority";
+
+    const client = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected successfully!");
   } catch (error) {
-    console.log(error);
-    console.log("MongoDB connection failed");
+    console.error("Error:", error.message);
+    console.error("MongoDB connection failed");
   }
 }
-module.exports = connectDB;
 
-// "mongodb://127.0.0.1:27017/expenseTracker" pass:923IKYHSTB3Reazz
+module.exports = connectDB;
